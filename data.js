a15d1778af8672e5c7dc61a2d5192e449e8f7271
ayoolaayodele <ayodeleayoola264@gmail.com>
2021-02-18 04:01:38 -0800

    first commit

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    url:
+      "https://v16-web.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c001/f82feab4ca6141e2ab53d9970784374a/?a=1988&br=6326&bt=3163&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1613594817&l=202102171446130101902192203448C5A2&lr=tiktok_m&mime_type=video_mp4&pl=0&policy=2&qs=0&rc=anY1aHhveG9reTMzPDczM0ApNjM4ZzY6ZWQ0N2U4NDk3Z2dhMi0xMWUzLXNfLS1gMTZzczQzNl4tM14yMDQwYjE2Ly46Yw%3D%3D&signature=d5a46a3a47ec6354a272624fc21a78b7&tk=tt_webid_v2&vl=&vr=",
+    channel: "kwinlammie",
+    description: "call me baddest",
+    song: "aint on",
+    likes: "321",
+    messages: "123",
+    shares: "13",
+  },
+  {
+    url:
+      "https://v16-web.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c004/88b9d4edf0db412681e0c88af03b3be1/?a=1988&br=3334&bt=1667&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1613608146&l=20210217182851010190176042295EE2EC&lr=tiktok_m&mime_type=video_mp4&pl=0&policy=2&qs=0&rc=MzxsZGc7bHE3MzMzNTczM0ApNDhlN2Q2NTw7N2Q0N2Y6O2dfYXA2MXBvZWNgLS1hMTZzc18tYi4uMi1jMy0xLi9eXi46Yw%3D%3D&signature=425358c27cb15a53c6e44089c43b031f&tk=tt_webid_v2&vl=&vr=",
+    channel: "e4ma",
+    description: "It works",
+    song: "forever",
+    likes: "65",
+    messages: "45",
+    shares: "11",
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+// middlewares
+app.use(express.json());
+
+// DB config
+const connection_url =
+  "mongodb+srv://pecker:pecker123@cluster0.ccd3i.mongodb.net/tiktok?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(connection_url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+//ConnectDB
+connectDB();
+
+// api endpoints
+app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+
+app.get("/v2/posts", (err, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
